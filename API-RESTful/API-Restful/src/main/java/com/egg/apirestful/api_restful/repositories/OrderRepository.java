da59e3e74d45995990faf1efd8ba554c48a9e8ef
Santiago Tombolini <santitombo@gmail.com>
2023-12-27 20:03:13 -0300

    repos

--- API-RESTful/API-Restful/src/main/java/com/egg/apirestful/api_restful/repositories/OrderRepository.java
+++ API-RESTful/API-Restful/src/main/java/com/egg/apirestful/api_restful/repositories/OrderRepository.java
@@ -1,9 +1,9 @@
 package com.egg.apirestful.api_restful.repositories;
 
-import com.egg.apirestful.api_restful.entities.Product;
+import com.egg.apirestful.api_restful.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface productRepository extends JpaRepository<Product, String> {
+public interface OrderRepository extends JpaRepository<Order, String> {
 }
--- API-RESTful/API-Restful/src/main/java/com/egg/apirestful/api_restful/repositories/ProductRepository.java
+++ API-RESTful/API-Restful/src/main/java/com/egg/apirestful/api_restful/repositories/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.egg.apirestful.api_restful.repositories;
 
-import com.egg.apirestful.api_restful.entities.Order;
+import com.egg.apirestful.api_restful.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface orderRepository extends JpaRepository<Order, String> {
+public interface ProductRepository extends JpaRepository<Product, String> {
 }
